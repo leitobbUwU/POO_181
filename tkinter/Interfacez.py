363e9ace11488c3723eeebdc1c776ddfbdf569b3
leitobbUwU <121040159@upq.edu.mx>
2023-03-07 09:26:56 -0600

    intento de interfaz

--- tkinter/Interfacez.py
+++ tkinter/Interfacez.py
@@ -1,11 +1,19 @@
 import tkinter as tk
 from tkinter import messagebox, Label, Button
 
+
+def Interfaz1():
+    ventana= tk.Tk()
+    ventana=ventana
+    ventana.title("Practica 11.3 Frames")
+    ventana.geometry("800x600")
+
 ventana= tk.Tk()
 ventana=ventana
 ventana.title("Practica 11.3 Frames")
 ventana.geometry("800x600")
 
-titulo2 =Label(ventana, text="Contraseña:", font=("Helvetica", 30))
-titulo2.pack(fill=tk.X, padx=20, pady=5)
+botonLog=Button(ventana, text="Boton Azul", fg="red", bg="#00ccff", font=("Helvetica", 15), command=Interfaz1)
+botonLog.pack()
+
 ventana.mainloop()

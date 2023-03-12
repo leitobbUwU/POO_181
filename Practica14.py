bab97e7bcece8726ac2bfc04cf34ca8ad095b227
leitobbUwU <121040159@upq.edu.mx>
2023-03-12 11:23:00 -0600

    Creacion de funciones para los messagebox

--- Practica14.py
+++ Practica14.py
@@ -33,32 +33,87 @@ class Window:
         self.Saldo = tk.Entry(ventana, font=("Helvetica", 30), bg='#00ccff')
         self.Saldo.pack(padx=20,pady=10)
         
-        self.Generar= tk.Button(cosa, text="Guardar datos", fg="#000000", bg="#FFFFFF", font=("Roman", 15), command=self.registro)
+        self.Generar= tk.Button(cosa, text="Guardar datos", fg="#000000", bg="#FFFFFF", font=("Roman", 15), command=self.RealizarReg)
         self.Generar.pack()
         
-        self.Generar= tk.Button(cosa, text="Consultar", fg="#000000", bg="#FFFFFF", font=("Roman", 15), command=self.MostrarReg)
+        self.Generar= tk.Button(cosa, text="Consultar", fg="#000000", bg="#FFFFFF", font=("Roman", 15), command=self.MostrarRegistro)
         self.Generar.pack()
         
-    def registro(self):
+    def registroTitular(self):
         User = self.Titular.get()
-        Count= self.NoCuenta.get()
-        Age= self.Edad.get()
-        Balance= self.Saldo.get()
-        
-        Registrar=datos(User, Count, Age, Balance) # type: ignore
+        Registrar=datos(User) # type: ignore
         if Registrar.Guardar():
             messagebox.showinfo("Exito", "Registro de usuario exitoso " + User)
         else:
-            messagebox.showerror("El usuario esta vacio")
+            messagebox.showerror("Error", "El usuario esta vacio.")
             
-    def MostrarReg(self):
-        Registro=self.Titular.get(), self.NoCuenta.get(), self.Edad.get(), self.Saldo.get()
-        
-        consulta=datos(Registro)
-        if consulta.Guardar():
-            messagebox.showinfo("Registro", "El registro es: " + Registro) # type: ignore
+    def registroCuenta(self):
+        Count= self.NoCuenta.get()
+        Registrar=datos(Count)
+        if Registrar.Guardar():
+            messagebox.showinfo("Exito", "La cuenta " + Count +" se registro exitosamente")
         else:
-            messagebox.showerror("No hay datos")
+            messagebox.showerror("Error", "La cuenta esta vacia.")
+            
+    def registroEdad(self):
+        Age= self.Edad.get()
+        Registrar=datos(Age) # type: ignore
+        if Registrar.Guardar():
+            messagebox.showinfo("Exito", "Edad del titular: " + Age)
+        else:
+            messagebox.showerror("Error", "La edad está Vacia")
+            
+    def registroSaldo(self):
+        Balance= self.Saldo.get()
+        Registrar=datos(Balance) # type: ignore
+        if Registrar.Guardar():
+            messagebox.showinfo("Exito", "El saldo es de: " + Balance)
+        else:
+            messagebox.showerror("Error", "El campo saldo está vacío")
+            
+    def RealizarReg(self):
+        self.registroTitular()
+        self.registroCuenta()
+        self.registroEdad()
+        self.registroSaldo()
+            
+    def MostrarUsu(self):
+        RegistroUsu=self.Titular.get()
+        consultaUsu=datos(RegistroUsu)
+        if consultaUsu.Guardar():
+            messagebox.showinfo("Registro", "El usuario es: " + RegistroUsu) # type: ignore
+        else:
+            messagebox.showerror("Error", "No hay registro en usuario.")
+            
+    def MostrarCoun(self):
+        RegistroCoun=self.NoCuenta.get()
+        consultaCoun=datos(RegistroCoun)
+        if consultaCoun.Guardar():
+            messagebox.showinfo("Registro", "La cuenta es: " + RegistroCoun) # type: ignore
+        else:
+            messagebox.showerror("Error", "No hay registro en cuenta.")
+            
+    def MostrarEdad(self):
+        RegistroEdad=self.Edad.get()
+        consultaEdad=datos(RegistroEdad)
+        if consultaEdad.Guardar():
+            messagebox.showinfo("Registro", "La edad es: " + RegistroEdad) # type: ignore
+        else:
+            messagebox.showerror("Error", "No hay registro en edad.")
+            
+    def MostrarSaldo(self):
+        RegistroSaldo=self.Saldo.get()
+        consultaSaldo=datos(RegistroSaldo)
+        if consultaSaldo.Guardar():
+            messagebox.showinfo("Registro", "El saldo es: " + RegistroSaldo) # type: ignore
+        else:
+            messagebox.showerror("Error", "No hay registro en saldo.")
+            
+    def MostrarRegistro(self):
+        self.MostrarUsu()
+        self.MostrarCoun()
+        self.MostrarEdad()
+        self.MostrarSaldo()
         
 ventana= tk.Tk()
 Mostrar=Window(ventana)

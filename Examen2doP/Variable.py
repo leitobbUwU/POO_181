c6900ba56f86ccb639e90c4f8046a27ffff9b8a8
leitobbUwU <121040159@upq.edu.mx>
2023-03-15 18:52:42 -0600

    commit de las 6:50

--- Examen2doP/Variable.py
+++ Examen2doP/Variable.py
@@ -1,6 +1,5 @@
 import random
 
-def aleatorio(string):
-    letras = string.ascii_lowercase
-    password = ''.join(random.choice(letras) for _ in range(2))
+def aleatorio(letras):
+    password = ''.join(random.choice(letras))
     return password
